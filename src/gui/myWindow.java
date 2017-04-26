acee700e47afaa52f28d75c6b1bfa9b88b5834ac
bajmer <marcin.bala@interia.pl>
2017-04-26 16:23:20 +0200

    GUI- adding buttons

--- src/gui/myWindow.java
+++ src/gui/myWindow.java
@@ -1,18 +1,41 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
 
 /**
  * Created by mbala on 26.04.17.
  */
-public class myWindow extends JFrame {
+public class myWindow extends JFrame implements ActionListener {
+
+    private JButton bLoad, bExit;
 
     public myWindow() {
         setSize(800,600);
         setTitle("VRP System");
         setLayout(null);
-        JButton button = new JButton("Load data");
-        button.setBounds(50,50,300,30);
-        add(button);
+
+        bLoad = new JButton("Load data");
+        bLoad.setBounds(50,50,300,30);
+        add(bLoad);
+        bLoad.addActionListener(this);
+
+        bExit = new JButton("Exit");
+        bExit.setBounds(50,100,300,30);
+        add(bExit);
+        bExit.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source == bLoad) {
+            System.out.println(new Date());
+        } else if (source == bExit) {
+            dispose();
+        }
+
     }
 }
