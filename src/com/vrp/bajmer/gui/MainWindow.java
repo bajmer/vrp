d662bf0e5731251ef56ec8c437a2f07f27ff07d5
Marcin Bala <marcin.bala@javeo.eu>
2017-07-20 13:26:09 +0200

    Dodanie okien z tabelami klientów i segmentów trasy

--- src/com/vrp/bajmer/gui/MainWindow.java
+++ src/com/vrp/bajmer/gui/MainWindow.java
@@ -6,6 +6,8 @@ import com.vrp.bajmer.algorithm.Second_Algorithm;
 import com.vrp.bajmer.algorithm.Third_Algorithm;
 import com.vrp.bajmer.core.Customer;
 import com.vrp.bajmer.core.Problem;
+import com.vrp.bajmer.core.RouteSegment;
+import com.vrp.bajmer.core.Storage;
 import com.vrp.bajmer.io.FileReader;
 import com.vrp.bajmer.network.DistanceMatrix;
 import com.vrp.bajmer.network.Geolocation;
@@ -14,12 +16,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.text.NumberFormatter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.text.NumberFormat;
+import java.util.Vector;
 
 /**
  * Created by mbala on 26.04.17.
@@ -27,14 +31,6 @@ import java.text.NumberFormat;
 public class MainWindow extends JFrame implements ActionListener {
 
     private static final Logger logger = LogManager.getLogger(MainWindow.class);
-
-    /*private static final int xPos = 20;
-    private static final int xDiff = 0;
-    private static final int yPos = 20;
-    private static final int yDiff = 30;
-    private static final int xSize = 300;
-    private static final int ySize = 20;*/
-
 
     private JButton bLoad, bGetDistance, bCalculate, bShowMap, bExit;
     private JComboBox<String> boxAlgorithm;
@@ -47,6 +43,8 @@ public class MainWindow extends JFrame implements ActionListener {
     private JFormattedTextField fNumberOfVehicles;
     private JFormattedTextField fWeightLimit;
     private JFormattedTextField fSizeLimit;
+    private JTable tCustomersData;
+    private JTable tRouteSegments;
     private JLabel mapImage;
     private String mapWindowName;
 
@@ -57,9 +55,10 @@ public class MainWindow extends JFrame implements ActionListener {
         JPanel mainPanel = new JPanel(new GridBagLayout());
         GridBagConstraints c1 = new GridBagConstraints();
         c1.anchor = GridBagConstraints.CENTER;
-        c1.insets = new Insets(5, 1, 5, 1);
+        c1.insets = new Insets(5, 5, 5, 5);
         c1.fill = GridBagConstraints.HORIZONTAL;
 
+        //**********button "Load Data"***************
         c1.gridx = 0;
         c1.gridy = 0;
         c1.gridwidth = 2;
@@ -67,6 +66,7 @@ public class MainWindow extends JFrame implements ActionListener {
         bLoad.addActionListener(this);
         mainPanel.add(bLoad, c1);
 
+//**********button "Get distance matrix"**************
         c1.gridx = 0;
         c1.gridy = 1;
         c1.gridwidth = 2;
@@ -75,21 +75,21 @@ public class MainWindow extends JFrame implements ActionListener {
         bGetDistance.setEnabled(false);
         mainPanel.add(bGetDistance, c1);
 
-//        JPanel paramPanel = new JPanel();
-//        paramPanel.setBorder(BorderFactory.createTitledBorder("General parameters"));
-//        paramPanel.setLayout(new GridBagLayout());
-
+//************General parameters**********************
         NumberFormat integerFormat = NumberFormat.getIntegerInstance();
         integerFormat.setGroupingUsed(false);
         NumberFormatter numberFormatter = new NumberFormatter(integerFormat);
         numberFormatter.setValueClass(Integer.class);
         numberFormatter.setAllowsInvalid(false);
 
-        c1.insets = new Insets(1, 1, 1, 1);
+        Font propertiesFont = new Font(Font.DIALOG, Font.PLAIN, 12);
+
+        c1.insets = new Insets(1, 5, 1, 5);
         c1.gridx = 0;
         c1.gridy = 2;
         c1.gridwidth = 1;
         algorithmIDText = new JLabel("Algorithm ID:");
+        algorithmIDText.setFont(propertiesFont);
         mainPanel.add(algorithmIDText, c1);
 
         c1.gridx = 1;
@@ -102,6 +102,7 @@ public class MainWindow extends JFrame implements ActionListener {
         c1.gridy = 3;
         c1.gridwidth = 1;
         numberOfVehiclesText = new JLabel("Number of vehicles:");
+        numberOfVehiclesText.setFont(propertiesFont);
         mainPanel.add(numberOfVehiclesText, c1);
 
         c1.gridx = 1;
@@ -114,6 +115,7 @@ public class MainWindow extends JFrame implements ActionListener {
         c1.gridy = 4;
         c1.gridwidth = 1;
         weightLimitText = new JLabel("Vehicle capacity [kg]:");
+        weightLimitText.setFont(propertiesFont);
         mainPanel.add(weightLimitText, c1);
 
         c1.gridx = 1;
@@ -126,6 +128,7 @@ public class MainWindow extends JFrame implements ActionListener {
         c1.gridy = 5;
         c1.gridwidth = 1;
         sizeLimitText = new JLabel("Vehicle capacity [m3]:");
+        sizeLimitText.setFont(propertiesFont);
         mainPanel.add(sizeLimitText, c1);
 
         c1.gridx = 1;
@@ -134,7 +137,16 @@ public class MainWindow extends JFrame implements ActionListener {
         fSizeLimit = new JFormattedTextField(numberFormatter);
         mainPanel.add(fSizeLimit, c1);
 
-        c1.insets = new Insets(5, 1, 5, 1);
+//************Text fields mock****************************
+        if (true) {
+            fAlgorithmID.setText("1");
+            fNumberOfVehicles.setText("1");
+            fWeightLimit.setText("1400"); //dla Mercedes Sprinter 2017, Standard z rozstawem osi 3665 mm, z dachem normalnym
+            fSizeLimit.setText("9"); //dla Mercedes Sprinter 2017, Standard z rozstawem osi 3665 mm, z dachem normalnym
+        }
+
+//**********algorithms list*******************************
+        c1.insets = new Insets(5, 5, 5, 5);
         c1.gridx = 0;
         c1.gridy = 6;
         c1.gridwidth = 2;
@@ -147,6 +159,7 @@ public class MainWindow extends JFrame implements ActionListener {
         boxAlgorithm.setEnabled(false);
         mainPanel.add(boxAlgorithm, c1);
 
+//************Button "Calculate"*********************************
         c1.gridx = 0;
         c1.gridy = 7;
         c1.gridwidth = 2;
@@ -172,136 +185,8 @@ public class MainWindow extends JFrame implements ActionListener {
         bExit.addActionListener(this);
         mainPanel.add(bExit, c1);
 
-
-
-        /*c1.gridx = 0;
-        c1.gridy = 1;
-        newPanel.add(labelPassword, c1);
-
-        c1.gridx = 1;
-        newPanel.add(fieldPassword, c1);
-
-        c1.gridx = 0;
-        c1.gridy = 2;
-        c1.gridwidth = 2;
-        c1.anchor = GridBagConstraints.CENTER;
-        newPanel.add(buttonLogin, c1);*/
-
-        // set border for the panel
-//        newPanel.setBorder(BorderFactory.createTitledBorder(
-//                BorderFactory.createEtchedBorder(), "Login Panel"));
-
-        // add the panel to this frame
-        add(mainPanel);
-        /*
-//**********button "Load Data"***************
-        bLoad = new JButton("Load data");
-//        bLoad.setBounds(xPos, yPos, xSize, ySize);
-
-        add(bLoad);
-
-//**********button "Get distance matrix"**************
-        bGetDistance = new JButton("Get distance matrix");
-        bGetDistance.setBounds(xPos, yPos + yDiff, xSize, ySize);
-        bGetDistance.addActionListener(this);
-        bGetDistance.setEnabled(false);
-        add(bGetDistance);
-
-//************General parameters******************
-        paramPanel = new JPanel();
-        paramPanel.setBorder(BorderFactory.createTitledBorder("General parameters"));
-        paramPanel.setBounds(xPos, yPos + 2 * yDiff, xSize, 130);
-        //paramPanel.setEnabled(true);
-        add(paramPanel);
-
-        NumberFormat integerFormat = NumberFormat.getIntegerInstance();
-        integerFormat.setGroupingUsed(false);
-        NumberFormatter numberFormatter = new NumberFormatter(integerFormat);
-        numberFormatter.setValueClass(Integer.class);
-        numberFormatter.setAllowsInvalid(false);
-
-        JLabel algorithmIDText = new JLabel("Algorithm ID:");
-        algorithmIDText.setBounds(xPos, yPos + 3 * yDiff, 160, 20);
-        algorithmIDText.setHorizontalAlignment(SwingConstants.LEFT);
-        paramPanel.add(algorithmIDText);
-//        add(algorithmIDText);
-        fAlgorithmID = new JFormattedTextField(numberFormatter);
-        fAlgorithmID.setBounds(180, yPos + 3 * yDiff, 40, 20);
-        fAlgorithmID.setEnabled(false);
-        paramPanel.add(fAlgorithmID);
-//        add(fAlgorithmID);
-
-        JLabel numberOfVehiclesText = new JLabel("Number of vehicles:");
-        numberOfVehiclesText.setBounds(xPos, yPos + 4 * yDiff, 160, 20);
-        numberOfVehiclesText.setHorizontalAlignment(SwingConstants.LEFT);
-        paramPanel.add(numberOfVehiclesText);
-//        add(numberOfVehiclesText);
-        fNumberOfVehicles = new JFormattedTextField(numberFormatter);
-        fNumberOfVehicles.setBounds(180, yPos + 4 * yDiff, 40, 20);
-        fNumberOfVehicles.setEnabled(false);
-        paramPanel.add(fNumberOfVehicles);
-//        add(fNumberOfVehicles);
-
-        JLabel weightLimitText = new JLabel("Vehicle capacity [kg]:");
-        weightLimitText.setBounds(xPos, yPos + 5 * yDiff, 160, 20);
-        weightLimitText.setHorizontalAlignment(SwingConstants.LEFT);
-        paramPanel.add(weightLimitText);
-//        add(weightLimitText);
-        fWeightLimit = new JFormattedTextField(numberFormatter);
-        fWeightLimit.setBounds(180, yPos + 5 * yDiff, 40, 20);
-        fWeightLimit.setEnabled(false);
-        paramPanel.add(fWeightLimit);
-//        add(fWeightLimit);
-
-        JLabel sizeLimitText = new JLabel("Vehicle capacity [m3]:");
-        sizeLimitText.setBounds(xPos, yPos + 6 * yDiff, 160, 20);
-        sizeLimitText.setHorizontalAlignment(SwingConstants.LEFT);
-        paramPanel.add(sizeLimitText);
-//        add(sizeLimitText);
-        fSizeLimit = new JFormattedTextField(numberFormatter);
-        fSizeLimit.setBounds(180, yPos + 6 * yDiff, 40, 20);
-        fSizeLimit.setEnabled(false);
-        paramPanel.add(fSizeLimit);
-//        add(fSizeLimit);
-//        add(paramPanel);
-
-//**********algorithms list********************
-        boxAlgorithm = new JComboBox<>();
-        boxAlgorithm.addItem("Clark-Wright");
-        boxAlgorithm.addItem("Second");
-        boxAlgorithm.addItem("Third");
-        boxAlgorithm.setBounds(xPos, yPos + 6 * yDiff, xSize, ySize);
-        boxAlgorithm.setSelectedIndex(0);
-        boxAlgorithm.addActionListener(this);
-        boxAlgorithm.setEnabled(false);
-        add(boxAlgorithm);
-
-//************Text fields mock****************************
-        if (false) {
-            fAlgorithmID.setText("1");
-            fNumberOfVehicles.setText("1");
-            fWeightLimit.setText("1400"); //dla Mercedes Sprinter 2017, Standard z rozstawem osi 3665 mm, z dachem normalnym
-            fSizeLimit.setText("9"); //dla Mercedes Sprinter 2017, Standard z rozstawem osi 3665 mm, z dachem normalnym
-        }
-//************Button "Calculate"*********************************
-        bCalculate = new JButton("Calculate");
-        bCalculate.setBounds(xPos, yPos + 7 * yDiff, xSize, ySize);
-        bCalculate.addActionListener(this);
-        bCalculate.setEnabled(false);
-        add(bCalculate);
-
-//***********Button "Show solution on map"************************
-        bShowMap = new JButton("Show solution on map");
-        bShowMap.setBounds(xPos, yPos + 8 * yDiff, xSize, ySize);
-        bShowMap.addActionListener(this);
-        bShowMap.setEnabled(false);
-        add(bShowMap);
-
-//**********Button "Exit"********************************
-        bExit = new JButton("Exit");
-        bExit.setBounds(xPos, yPos + 9 * yDiff, xSize, ySize);
-        bExit.addActionListener(this);
-        add(bExit);*/
+        this.add(mainPanel);
+        this.pack();
     }
 
     @Override
@@ -323,6 +208,8 @@ public class MainWindow extends JFrame implements ActionListener {
                 File customersInputFile = fileReader.chooseFile(this);
                 if (customersInputFile != null) {
                     fileReader.readFile(customersInputFile);
+                    createCustomersWindow();
+                    createRouteSegmentsWindow();
                     bGetDistance.setEnabled(true);
                 }
             } catch (Exception ex) {
@@ -334,6 +221,8 @@ public class MainWindow extends JFrame implements ActionListener {
                 geolocation.downloadCustomersCoordinates();
                 DistanceMatrix distanceMatrix = new DistanceMatrix();
                 distanceMatrix.downloadDistanceMatrix();
+                updateCustomersWindow();
+                updateRouteSegmentsWindow();
                 bGetDistance.setEnabled(false);
                 boxAlgorithm.setEnabled(true);
 
@@ -385,16 +274,126 @@ public class MainWindow extends JFrame implements ActionListener {
             }
 
         } else if (source == bShowMap) {
-            JFrame mapWindow = new JFrame(mapWindowName);
-            mapWindow.setSize(640, 640);
-            mapWindow.setLocation(240, 0);
-            mapWindow.setVisible(true);
-            mapWindow.add(mapImage);
+            createMapWindow();
         } else if (source == bExit) {
             dispose();
             logger.info("Application stopped.");
             logger.info("*********************************************************************************************************************************************");
             System.exit(0);
+        }
+    }
+
+    private void createMapWindow() {
+        JFrame mapWindow = new JFrame(mapWindowName);
+        mapWindow.setSize(640, 640);
+        mapWindow.setLocation(240, 0);
+        mapWindow.setVisible(true);
+        mapWindow.add(mapImage);
+    }
+
+    private void createCustomersWindow() {
+        Vector<String> columns = new Vector<>();
+        columns.add("ID");
+        columns.add("Address");
+        columns.add("Latitude");
+        columns.add("Longitude");
+        columns.add("Pack. weight");
+        columns.add("Pack. size");
+        columns.add("Min. hour");
+        columns.add("Max. hour");
+
+        Vector<Vector<String>> data = new Vector<>();
+
+        for (Customer c : Storage.getCustomerList()) {
+            Vector<String> row = new Vector<>();
+            row.add(Integer.toString(c.getId()));
+            row.add(c.getAddress());
+            row.add(Double.toString(c.getLatitude()));
+            row.add(Double.toString(c.getLongitude()));
+            row.add(Double.toString(c.getPackageWeight()));
+            row.add(Double.toString(c.getPackageSize()));
+            row.add(c.getMinDeliveryHour());
+            row.add(c.getMaxDeliveryHour());
+
+            data.add(row);
+        }
+
+        tCustomersData = new JTable(data, columns);
+        tCustomersData.setFont(new Font(Font.DIALOG, Font.PLAIN, 10));
+        tCustomersData.getColumnModel().getColumn(0).setPreferredWidth(10);
+        tCustomersData.getColumnModel().getColumn(1).setPreferredWidth(250);
+        tCustomersData.getColumnModel().getColumn(2).setPreferredWidth(20);
+        tCustomersData.getColumnModel().getColumn(3).setPreferredWidth(20);
+        tCustomersData.getColumnModel().getColumn(4).setPreferredWidth(20);
+        tCustomersData.getColumnModel().getColumn(5).setPreferredWidth(20);
+        tCustomersData.getColumnModel().getColumn(6).setPreferredWidth(20);
+        tCustomersData.getColumnModel().getColumn(7).setPreferredWidth(20);
+
+        JScrollPane spCustomersData = new JScrollPane(tCustomersData);
+
+        JFrame customersWindow = new JFrame("Customers");
+        customersWindow.setSize(800, 135);
+        customersWindow.setLocation(200, 0);
+        customersWindow.setVisible(true);
+        customersWindow.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        customersWindow.add(spCustomersData);
+    }
+
+    private void updateCustomersWindow() {
+        int rowIndex = 0;
+        for (Customer c : Storage.getCustomerList()) {
+            tCustomersData.setValueAt(Double.toString(c.getLatitude()), rowIndex, 2);
+            tCustomersData.setValueAt(Double.toString(c.getLongitude()), rowIndex, 3);
+            rowIndex++;
+        }
+    }
+
+    private void createRouteSegmentsWindow() {
+        Vector<String> columns = new Vector<>();
+        columns.add("From");
+        columns.add("To");
+        columns.add("Distance [km]");
+        columns.add("Duration [min]");
+
+        Vector<Vector<String>> data = new Vector<>();
+
+        for (RouteSegment r : Storage.getRouteSegmentsList()) {
+            Vector<String> row = new Vector<>();
+            row.add(r.getSrc().getAddress());
+            row.add(r.getDst().getAddress());
+            row.add(Double.toString(r.getDistance()));
+            row.add(Double.toString(r.getDuration()));
+
+            data.add(row);
+        }
+
+        tRouteSegments = new JTable(data, columns);
+        tRouteSegments.setFont(new Font(Font.DIALOG, Font.PLAIN, 10));
+        tRouteSegments.getColumnModel().getColumn(0).setPreferredWidth(210);
+        tRouteSegments.getColumnModel().getColumn(1).setPreferredWidth(210);
+        tRouteSegments.getColumnModel().getColumn(2).setPreferredWidth(50);
+        tRouteSegments.getColumnModel().getColumn(3).setPreferredWidth(50);
+
+        JScrollPane spCustomersData = new JScrollPane(tRouteSegments);
+
+        JFrame customersWindow = new JFrame("Route segments");
+        customersWindow.setSize(800, 130);
+        customersWindow.setLocation(200, 185);
+        customersWindow.setVisible(true);
+        customersWindow.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        customersWindow.add(spCustomersData);
+    }
+
+    private void updateRouteSegmentsWindow() {
+        DefaultTableModel tableModel = (DefaultTableModel) tRouteSegments.getModel();
+        for (RouteSegment r : Storage.getRouteSegmentsList()) {
+            Vector<String> row = new Vector<>();
+            row.add(r.getSrc().getAddress());
+            row.add(r.getDst().getAddress());
+            row.add(Double.toString(r.getDistance()));
+            row.add(Double.toString(r.getDuration()));
+
+            tableModel.addRow(row);
         }
     }
 }
